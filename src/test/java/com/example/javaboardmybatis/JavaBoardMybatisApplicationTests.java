08bcfcd3598eee87fbb39bcbe7da3ecaa94ac4a7
seokhwan2 <waw7095@gmail.com>
2024-02-05 01:28:33 +0900

    springboot-jsp-mybatis

---
+++ src/test/java/com/example/javaboardmybatis/JavaBoardMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaboardmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBoardMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
